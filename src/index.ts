4ff12114384c1e928a6c60ac36ad46d625ddf45c
LAPTOP-GQT1FNKP\NZR <nizar.adian.n@gmail.com>
2023-01-23 00:53:44 +0700

    test express worked

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,42 @@
 import dotenv from 'dotenv';
+import express, {Application, Request, Response} from 'express';
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import compression from "compression";
+import helmet from "helmet";
+import cors from "cors";
+
+
+class App {
+    public app: Application;
+
+
+    constructor() {
+        this.app = express();
+        this.plugins();
+        this.routes();
+    }
+
+    protected plugins():void {
+        this.app.use(bodyParser.json());
+        this.app.use(morgan("dev"));
+        this.app.use(compression());
+        this.app.use(helmet());
+        this.app.use(cors());
+    }
+
+    protected routes():void {
+        this.app.get('/', (req: Request, res: Response) => {
+            res.send("Test API Worked");
+        });
+    }
+}
 
 dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = new App().app;
+app.listen(PORT,() => {
+    console.log("Run on Port " + PORT);
+});
 
-console.log("Run on Environment " + process.env.NODE_ENV);
+
